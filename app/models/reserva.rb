eb4aefeb66ea9962aa464e212df7eadc8ed4f717
Felipe Gomez <fgomeza17@gmail.com>
2022-02-25 20:04:32 -0300

    modificacion de condiciones de reserva

--- app/models/reserva.rb
+++ app/models/reserva.rb
@@ -3,14 +3,16 @@ class Reserva < ApplicationRecord
     private
     def overlapping
         result = true
-
         if self.start_time.blank? || self.end_time.blank?
             errors.add(:start_time, 'it overlaps another reservation')
             puts "if7"
-
+        
         else
+            puts "inicio del else"
             Reserva.pluck(:start_time, :end_time, :id_cabana).each do |date_range|
+                puts "antes del ciclo"
                 if date_range[2] == self.id_cabana
+                    puts "entro al ciclo"
                     if self.start_time >= self.end_time
                         errors.add(:start_time, 'it overlaps another reservation')
                         puts "if1", self.start_time, date_range[0], self.end_time, date_range[1]
